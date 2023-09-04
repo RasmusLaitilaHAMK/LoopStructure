a31123a7ea933b1395c0213e931ac7571f1421f1
Rasmus <rasmus.laitila@student.hamk.fi>
2023-09-04 10:17:10 +0300

    Added For and  While Loop

--- LOOP/src/main.java
+++ LOOP/src/main.java
@@ -3,7 +3,17 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello");
+		//System.out.println("Hello");
+		for (int count=1 ; count <= 10 ; count++)
+		{
+			System.out.println("Hello" + count);
+		}
+		int counter = 1;
+		while (counter <= 5)
+		{
+			System.out.println(counter);
+			counter++;
+		}
 	}
 
 }
