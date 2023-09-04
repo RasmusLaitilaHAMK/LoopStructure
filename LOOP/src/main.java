1aab5c94117962ac2879fe1ddfd9f596227857d2
Rasmus <rasmus.laitila@student.hamk.fi>
2023-09-04 10:28:18 +0300

    DoWhileLoop

--- LOOP/src/main.java
+++ LOOP/src/main.java
@@ -1,19 +1,39 @@
-
+import java.util.Random;
+import java.util.Scanner;
 public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//System.out.println("Hello");
-		for (int count=1 ; count <= 10 ; count++)
+		
+		//FOR LOOP
+//		for (int count=1 ; count <= 10 ; count++)
+//		{
+//			System.out.println("Hello" + count);
+//		}
+//		
+//		//WHILE LOOP
+//		int counter = 1;
+//		while (counter <= 5)
+//		{
+//			System.out.println(counter);
+//			counter++;
+//		}
+		
+		//DO WHILE LOOP
+		Random r = new Random();
+		Scanner in = new Scanner(System.in);
+		String playAgain;
+		do 
 		{
-			System.out.println("Hello" + count);
+			System.out.println("Throwing the dice");
+			System.out.println(r.nextInt(7)+1);
+			System.out.println("Play again? (Y/N)?");
+			playAgain = in.nextLine();
+			
 		}
-		int counter = 1;
-		while (counter <= 5)
-		{
-			System.out.println(counter);
-			counter++;
-		}
+		while (playAgain.equals("y"));
+		System.out.println("Thanks for playing");
 	}
 
 }
